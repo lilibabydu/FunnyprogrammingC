78587f8f9f78ef8e8e089ab611b024fcf7b4a6d8
lilibaby <lilibabydu@163.com>
2022-10-03 15:46:50 +0800

    第十一课

--- ch-7.cpp
+++ ch-7.cpp
@@ -1,28 +1,15 @@
 //#include <iostream>
 //using namespace std;
-//int main()
-//{
-//	int a,b,t;
-//	a=10;
-//	b=20;
-//	cout << "a="<<a<<"b="<<b<<endl;
-//	t=a;
-//	a=b;
-//	b=t;
-//	cout <<"a="<<a<<"b="<<b<<endl;
-//	return 0;
-// }
-
-#include <iostream>
-using namespace std;
 int main()
 {
-	int a,b;
-	a=100;
-	b=200;
-	a=b-a;
-	b-=a;
-	a+=b;
+	int a,b,t;
+	a=10;
+	b=20;
+	cout << "a="<<a<<"b="<<b<<endl;
+	t=a;
+	a=b;
+	b=t;
 	cout <<"a="<<a<<"b="<<b<<endl;
 	return 0;
  }
+
